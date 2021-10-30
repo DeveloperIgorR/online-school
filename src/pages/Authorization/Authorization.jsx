a43dc922d98aa2b76ac76831edb9b9a59008d5be
DeveloperIgorR <bratca@inbox.ru>
2021-10-30 23:26:45 +0300

    auth fix

--- src/pages/Authorization/Authorization.jsx
+++ src/pages/Authorization/Authorization.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Form, Input, Button, Checkbox } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
+// import { Form, Input, Button, Checkbox } from 'antd'
+// import { UserOutlined, LockOutlined } from '@ant-design/icons'
 // import a from './Authorization.module.css'
 
 
