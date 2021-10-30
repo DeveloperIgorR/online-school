2a5155b48d7a79a1e230178e07bbd98fc59c7955
DeveloperIgorR <bratca@inbox.ru>
2021-10-31 00:37:16 +0300

    auth fix

--- src/pages/Authorization/Authorization.jsx
+++ src/pages/Authorization/Authorization.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Form, Input} from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { Form, Input, Button, Checkbox } from 'antd'
+// import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import a from './Authorization.module.css'
 
 
@@ -14,25 +14,46 @@ const Authorization = () => {
             <h1 className={a.name}>ReactDevelopersSchool</h1>
             <h2 className={a.in}>Вход в личный кабинет</h2>
             <Form
-                // name="normal_login"
+                name="normal_login"
                 className={a.loginForm}
-                // initialValues={{ remember: true }}
+                initialValues={{ remember: true }}
             // onFinish={onFinish}
             >
-                
+                <Form.Item
+                    name="Логин:"
+                    rules={[{ required: true, message: 'Введите ваш Логин!' }]}
+                >
+                    <Input
+                        // prefix={<UserOutlined />}
+                        placeholder="Логин"
+                    />
+                </Form.Item>
 
                 <Form.Item
                     name="Пароль:"
                     rules={[{ required: true, message: 'Введите ваш Пароль!' }]}
                 >
                     <Input
-                        prefix={<LockOutlined />}
+                        // prefix={<LockOutlined />}
                         type="password"
                         placeholder="Пароль"
                     />
                 </Form.Item>
 
-                
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Запомнить меня</Checkbox>
+                    </Form.Item>
+                    <a>Забыли пароль?</a>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Войти
+                    </Button>
+                    Или
+                    <a >Зарегистрироваться!</a>
+                </Form.Item>
             </Form>
         </>
     );
