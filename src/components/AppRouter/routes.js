b01bdf117f78aa95a94a8ccfb157a05a3d6799a0
DeveloperIgorR <bratca@inbox.ru>
2021-10-30 12:30:37 +0300

    small fix

--- src/components/AppRouter/routes.js
+++ src/components/AppRouter/routes.js
@@ -1,4 +1,4 @@
-import Dashboard from "../../pages/Dashboard/Dashboard"
+// import Dashboard from "../../pages/Dashboard/Dashboard"
 import Modules from "../../pages/Modules/Modules"
 import Authorization from "../../pages/Authorization/Authorization"
 
