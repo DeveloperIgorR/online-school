9e0af3472e843628799ea403ffa7c6b6f75c4ef0
DeveloperIgorR <bratca@inbox.ru>
2021-10-30 12:40:40 +0300

    small fix

--- src/components/AppRouter/routes.js
+++ src/components/AppRouter/routes.js
@@ -1,10 +1,10 @@
 // import Dashboard from "../../pages/Dashboard/Dashboard"
-import Modules from "../../pages/Modules/Modules"
-import Authorization from "../../pages/Authorization/Authorization"
+// import Modules from "../../pages/Modules/Modules"
+// import Authorization from "../../pages/Authorization/Authorization"
 
 export const privateRoutes = [
     // {path: '/', component: Dashboard, exact:true},
-    {path: '/modules', component: Modules, exact:true},    
+    // {path: '/modules', component: Modules, exact:true},    
 ]
 
 export const publicRoutes = [
