1c134f18584193a68b252c8fce9b3e3e8b6eb7d9
DeveloperIgorR <bratca@inbox.ru>
2021-11-11 00:30:10 +0200

    auth fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React,{ useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 import { BrowserRouter } from "react-router-dom"
 import AppRouter from "./components/AppRouter/AppRouter"
 import { AppContext } from "./context/context"
+import { instance } from './services/instance'
 
 function App() {
 const[isAuth,setIsAuth] = useState(false)
+const [user,setUser] = useState(null)
+
+// useEffect(() => {
+//   authorizing()
+//    },[])
+
+// async function authorizing(){
+//  try{
+//  const response = await instance.get(`/authorizing`)    
+//    setIsAuth(true)
+//    setUser(response.data.user)
+//  } catch (e) {
+//      console.log(e)
+//  }
+// }
 
   return (
     <AppContext.Provider value={{
       isAuth,
-      setIsAuth
+      setIsAuth,
+      user,
+      setUser
     }}>
       <BrowserRouter>
         <AppRouter />
