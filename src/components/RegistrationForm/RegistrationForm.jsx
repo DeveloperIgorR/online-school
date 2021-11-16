bd547bfe06c79a61a5c804475fb82490765f13d9
DeveloperIgorR <bratca@inbox.ru>
2021-11-16 22:41:30 +0200

    regForm

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,8 +1,33 @@
+import { Form, Input } from 'antd'
+
 const RegistrationForm = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Form
+                name="normal_login"                
+                initialValues={{ remember: true }}
+                onFinish={userAuth}                
+            >
+                <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: 'Введите ваш email!' }]}
+                >
+                    <Input
+                        placeholder="Введите email"
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Введите ваш password!' }]}
+                >
+                    <Input                        
+                        placeholder="Введите пароль"
+                    />
+                </Form.Item>
+                
+            </Form>
+        </>
     )
 }
 
