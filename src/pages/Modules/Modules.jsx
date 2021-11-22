8cc1d7a9597e77cc6c162343f0465c1c650bed9e
DeveloperIgorR <bratca@inbox.ru>
2021-11-22 22:50:32 +0200

    modules fix

--- src/pages/Modules/Modules.jsx
+++ src/pages/Modules/Modules.jsx
@@ -7,8 +7,8 @@ const Modules = () => {
 
     const [visible, setVisible] = useState(false)
     const [confirmLoading, setConfirmLoading] = useState(false)
-    const modules = [{title:'HTML/CSS',color:'volcano'},{title:'JS',color:'violet'},
-    {title:'React',color:'cyan'}, {title:'Node',color:'green'},{title:'Базы данных',color:'yellow'}]
+    const modules = [{ title: 'HTML/CSS', color: 'volcano' }, { title: 'JS', color: 'violet' },
+    { title: 'React', color: 'cyan' }, { title: 'Node', color: 'green' }, { title: 'Базы данных', color: 'yellow' }]
 
     const handleOk = () => {
         setVisible(false)
@@ -28,39 +28,12 @@ const Modules = () => {
 
             <Row justify="start" align="middle" gutter={[8, 32]}>
                 <Col xs={{ span: 5, offset: 1 }} md={{ span: 5, offset: 2 }}>
-                    <Badge.Ribbon color="volcano">
-                        <Card title="HTML/CSS" size="default">
-
-                        </Card>
-                    </Badge.Ribbon>
-                </Col>
-                <Col xs={{ span: 5, offset: 1 }} md={{ span: 5, offset: 2 }}>
-                    <Badge.Ribbon color="violet">
-                        <Card title="JS" size="default">
-
-                        </Card>
-                    </Badge.Ribbon>
-                </Col>
-                <Col xs={{ span: 5, offset: 1 }} md={{ span: 5, offset: 2 }}>
-                    <Badge.Ribbon color="cyan">
-                        <Card title="React" size="default">
-
-                        </Card>
-                    </Badge.Ribbon>
-                </Col>
-                <Col xs={{ span: 5, offset: 1 }} md={{ span: 5, offset: 2 }}>
-                    <Badge.Ribbon color="green">
-                        <Card title="Node" size="default">
-
-                        </Card>
-                    </Badge.Ribbon>
-                </Col>
-                <Col xs={{ span: 5, offset: 1 }} md={{ span: 5, offset: 2 }}>
-                    <Badge.Ribbon color="yellow">
-                        <Card title="Базы данных" size="default">
-
-                        </Card>
-                    </Badge.Ribbon>
+                    {modules.map((item) =>
+                        <Badge.Ribbon color= {item.color} >
+                            <Card title= {item.title} size="default">
+                            </Card>
+                        </Badge.Ribbon>
+                    )}
                 </Col>
             </Row>
             <Modal
