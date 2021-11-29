2923a3935cfb6f5a3e8dbc4aa4eead8eac396b18
Павел Гуцу <>
2021-11-29 10:01:54 +0300

    added menu

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -3,6 +3,14 @@ import { Redirect, Route, Switch } from "react-router"
 import { useContext } from "react/cjs/react.development"
 import { AppContext } from "../../context/context"
 import { privateRoutes, publicRoutes } from "./routes"
+import { Layout, Menu, Row, Col, Button} from 'antd'
+import { ExportOutlined } from '@ant-design/icons'
+import d from '../../pages/Dashboard/Dashboard.module.css'
+import { Link } from 'react-router-dom'
+import TopHeader from '../../components/Header/Header'
+const { Header, Content, Footer, Sider } = Layout
+const { SubMenu } = Menu
+
 
 const AppRouter = () => {
 
@@ -15,7 +23,31 @@ const AppRouter = () => {
   return (
     isAuth || isAuthenticated()
       ?
-      <Switch>
+      <Layout style={{minHeight: '100vh' }}>
+      <Sider  >
+        <div className={d.logo}>
+          <h2>ReactDevelopers</h2>
+        </div>
+        <Menu  theme="dark" defaultSelectedKeys={['1']} mode="inline">          
+          <SubMenu key="sub1" title="Все пользователи">
+            <Menu.Item key="1">Динамика студентов</Menu.Item>
+            <Menu.Item key="2">Рейтинг благодарности</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="3" title="Модули">Модули<Link to='/modules' /></Menu.Item>
+          <Menu.Item key="4" title="Теория">Теория</Menu.Item>
+          <Menu.Item key="5" title="Задачи">Задачи</Menu.Item>
+          <Menu.Item key="6" title="Чеклисты">Чеклисты</Menu.Item>
+        </Menu>
+        {/* <Button className={d.button} icon={<ExportOutlined />} onClick={() => logOut()}>Выйти</Button> */}
+      </Sider>
+      <Layout className={d.siteLayout}>
+        <Header className={d.siteLayoutBackground}>
+          <TopHeader />
+        </Header>
+        <Content style={{ margin: '0 16px' }}>
+          <Row>
+            <Col xs={24} md={{ span: 20, offset: 2 }}>
+            <Switch>
         {privateRoutes.map(route =>
           <Route
             component={route.component}
@@ -25,6 +57,13 @@ const AppRouter = () => {
         )}
         <Redirect to='/' />
       </Switch>
+            </Col>
+          </Row>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Made by ©DIR</Footer>
+      </Layout>
+    </Layout>
+   
       :
       <Switch>
         {publicRoutes.map(route =>
