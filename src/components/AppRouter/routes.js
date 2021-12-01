39ad38a0202a42cad5c54a9458c431296abba4fd
DeveloperIgorR <bratca@inbox.ru>
2021-12-01 23:12:13 +0200

    routing fix

--- src/components/AppRouter/routes.js
+++ src/components/AppRouter/routes.js
@@ -1,16 +1,9 @@
 import Modules from "../../pages/Modules/Modules"
-import Theory from "../../pages/Theory/Theory"
-import Tasks from "../../pages/Tasks/Tasks"
-import Checklists from "../../pages/Checklists/Checklists"
 import Authorization from "../../pages/Authorization/Authorization"
-import MainTable from "../MainTable/MainTable"
 
 export const privateRoutes = [
-    {path: '/user/users', component: MainTable, exact:true},
-    {path: '/user/modules', component: Modules, exact:true},
-    {path: '/user/theory', component: Theory, exact:true},
-    {path: '/user/tasks', component: Tasks, exact:true},
-    {path: '/user/checklists', component: Checklists, exact:true}
+    {path: '/', component: Dashboard, exact:true},
+    {path: '/modules', component: Modules, exact:true} 
 ]
 
 export const publicRoutes = [
