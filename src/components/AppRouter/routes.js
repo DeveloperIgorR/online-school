63d1edf62c2574b2dc615dfa169c28e0ced84c4c
DeveloperIgorR <bratca@inbox.ru>
2021-12-04 22:48:16 +0200

    layout fix

--- src/components/AppRouter/routes.js
+++ src/components/AppRouter/routes.js
@@ -2,10 +2,12 @@ import Modules from "../../pages/Modules/Modules"
 import Authorization from "../../pages/Authorization/Authorization"
 import Dashboard from "../../pages/Dashboard/Dashboard"
 import Theory from "../../pages/Theory/Theory"
+import Tasks from "../../pages/Tasks/Tasks"
 
 export const privateRoutes = [
     {path: '/', component: Dashboard, exact:true},
     {path: '/modules', component: Modules, exact:true}, 
+    {path: '/tasks', component: Tasks, exact:true},
     {path: '/theory', component: Theory, exact:true},
 ]
 
